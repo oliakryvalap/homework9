8c1bc87eb812ac7c32b6b8dc73301248e11bcc5b
Olga <olia_tarasenko@inbox.ru>
2023-03-13 11:43:37 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Clear();
+
+System.Console.WriteLine(ReturnNumber(PutNumber("Введите m :")));
+
+int PutNumber(string message)
+{
+System.Console.WriteLine(message);
+int number = Convert.ToInt32(Console.ReadLine());
+return number;
+}
+string ReturnNumber(int m)
+{
+    if(m > 1) return (m - "");
+    else 
+    return m - " " + ReturnNumber(m - 1);
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+Console.WriteLine(SumNumber(PutNumber("Введите m :"), PutNumber("Введите n :")));
+
+int PutNumber(string message)
+{
+Console.WriteLine(message);
+int number = Convert.ToInt32(Console.ReadLine());
+return number;
+}
+string SumNumber(int n, int m)
+
+{
+    if (n == 0 || m == 0) return (0);
+    if (n < m || n > m)
+    {       
+        return (SumNumbers(n + 1, m)); 
+        Console.Write($"{n} ");
+    }
+    if (n==m)
+    {
+        Console.Write($"{n} "); return;
+    }
+}
+SumNumbers(m, n);
